b2d5ce2bf4d6d85be4d4be87f39eac85203bf053
Steve Partridge <srpartridge@gmail.com>
2018-07-15 13:59:19 -0700

    Add deps arg to specify which to update

--- _scripts/dev.sh
+++ _scripts/dev.sh
@@ -24,11 +24,21 @@ printf "Swagger Docs..."
 ./_scripts/swagger.sh
 printf "done.\n"
 
-if [[ "$1" == "deps" ]]; then
+echo $1
+case "$1" in 
+  *deps\=*)
+    ./_scripts/internal_deps.sh ${1//deps=/}
+    shift 
+    ;;
+  *deps*)
   ./_scripts/internal_deps.sh
-fi
+    shift 
+    ;;
+esac
+# if [[ "$1" == "deps" ]]; then
+# fi
 
 
 echo "Building..."
-go run cmd/service/*.go $@
-# go run $(ls -1 cmd/service/*.go | grep -v _test.go) $@
+# go run cmd/service/*.go $@
+go run $(ls -1 cmd/service/*.go | grep -v _test.go) $@
