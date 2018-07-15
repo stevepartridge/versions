03aa52bb1b36c7f49f826ecec513867bca37ebe6
Steve Partridge <srpartridge@gmail.com>
2018-07-15 14:03:32 -0700

    Fix Parse, add tests, add SHA helpers

--- utils.go
+++ utils.go
@@ -1,14 +1,19 @@
 package versions
 
 import (
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/hex"
 	"strings"
 )
 
 func Parse(ver string) (string, string, string) {
-	parts := strings.Split(ver, ".")
-	if len(parts) == 0 {
+	if ver == "" {
 		return "", "", ""
-	} else if len(parts) == 1 {
+	}
+
+	parts := strings.Split(ver, ".")
+	if len(parts) == 1 {
 		return parts[0], "", ""
 	} else if len(parts) == 2 {
 		return parts[0], parts[1], ""
@@ -16,6 +21,26 @@ func Parse(ver string) (string, string, string) {
 		return parts[0], parts[1], parts[2]
 	} else if len(parts) > 3 {
 		return parts[0], parts[1], strings.Join(parts[2:], "-")
+	} else {
+		return "", "", ""
 	}
-	return "", "", ""
+
+}
+
+func Sha1ToString(in []byte) string {
+
+	hasher := sha1.New()
+	hasher.Write(in)
+	out := hasher.Sum(nil)
+
+	return hex.EncodeToString(out)
+}
+
+func Sha256ToString(in []byte) string {
+
+	hasher := sha256.New()
+	hasher.Write(in)
+	out := hasher.Sum(nil)
+
+	return hex.EncodeToString(out)
 }
